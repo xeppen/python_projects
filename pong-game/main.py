218057e9530364960b135b06dbc6f4f37fc7ccbf
Sebastian Ljungberg <xeppen@gmail.com>
2022-04-28 20:31:56 +0200

    Break out into own class

--- pong-game/main.py
+++ pong-game/main.py
@@ -6,7 +6,20 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
-paddle = Paddle()
+
+
+
+
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
--- pong-game/paddle.py
+++ pong-game/paddle.py
@@ -6,3 +6,13 @@ class Paddle(Turtle):
         super().__init__()
         self.shape("square")
         self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
