9cbfc8aff26897f159bb41c45d44c263e3caa13a
Sebastian Ljungberg <xeppen@gmail.com>
2022-04-28 21:07:59 +0200

    Fix issue with paddle class

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 
 from paddle import Paddle
 
@@ -6,7 +6,19 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
-paddle = Paddle()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_running = True
+while game_is_running:
+    screen.update()
 
 screen.exitonclick()
