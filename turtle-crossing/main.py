0a52cca5d05e1f991d9adc9fe67f69d149b7cd6a
Sebastian Ljungberg <xeppen@gmail.com>
2022-04-29 13:46:49 +0200

    Turtle moving

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -1,14 +1,46 @@
-import time
 from turtle import Screen
 from player import Player
-from car_manager import CarManager
+import time
 from scoreboard import Scoreboard
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=800, height=600)
+screen.bgcolor("white")
+screen.title("Pong")
 screen.tracer(0)
 
-game_is_on = True
-while game_is_on:
+player = Player()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_running = True
+while game_is_running:
     time.sleep(0.1)
+
+    # # Bounce on Y
+    # if ball.ycor() > 280 or ball.ycor() < -280:
+    #     ball.bounce_y()
+    #
+    # # Bounce on r_ paddle
+    # if ball.distance(r_paddle) < 50 and ball.xcor() > 330:
+    #     ball.bounce_x()
+    #
+    # # Bounce on l_ paddle
+    # if ball.distance(l_paddle) < 50 and ball.xcor() < -330:
+    #     ball.bounce_x()
+    #
+    # # Right ball out
+    # if ball.xcor() > 380:
+    #     ball.reset_position()
+    #     scoreboard.r_point()
+    #
+    # # Right ball out
+    # if ball.xcor() < -380:
+    #     ball.reset_position()
+    #     scoreboard.l_point()
+
     screen.update()
+
+screen.exitonclick()
---
+++ turtle-crossing/player.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+    def go_up(self):
+        print("move")
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
