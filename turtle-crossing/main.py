b04f6b7a887add15b1c07907aff11ab967dc91a3
Sebastian Ljungberg <xeppen@gmail.com>
2022-04-29 19:30:30 +0200

    Turtle moving

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -1,4 +1,6 @@
 from turtle import Screen
+
+from car_manager import CarManager
 from player import Player
 import time
 from scoreboard import Scoreboard
@@ -11,6 +13,7 @@ screen.tracer(0)
 
 player = Player()
 scoreboard = Scoreboard()
+manager = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -18,28 +21,13 @@ screen.onkey(player.go_up, "Up")
 game_is_running = True
 while game_is_running:
     time.sleep(0.1)
+    manager.generate_car()
 
     # # Bounce on Y
     # if ball.ycor() > 280 or ball.ycor() < -280:
     #     ball.bounce_y()
-    #
-    # # Bounce on r_ paddle
-    # if ball.distance(r_paddle) < 50 and ball.xcor() > 330:
-    #     ball.bounce_x()
-    #
-    # # Bounce on l_ paddle
-    # if ball.distance(l_paddle) < 50 and ball.xcor() < -330:
-    #     ball.bounce_x()
-    #
-    # # Right ball out
-    # if ball.xcor() > 380:
-    #     ball.reset_position()
-    #     scoreboard.r_point()
-    #
-    # # Right ball out
-    # if ball.xcor() < -380:
-    #     ball.reset_position()
-    #     scoreboard.l_point()
+
+    manager.move()
 
     screen.update()
 
